f63ae9fdc1195cb0a5ea48d403ca14448a69f2cd
Zampilled <89923286+Zampilled@users.noreply.github.com>
2022-03-31 11:17:46 +0100

    migrating to postgres

--- ordersystem/ordersystem/wsgi.py
+++ ordersystem/ordersystem/wsgi.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from  whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ordersystem.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root="/media/")
