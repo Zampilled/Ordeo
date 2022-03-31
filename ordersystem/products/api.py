9bebd6c88c745234c7da0ce8f030e0fef15586a9
Zampilled <89923286+Zampilled@users.noreply.github.com>
2022-03-31 17:56:26 +0100

    Restricting Admin Capabilities

--- ordersystem/products/api.py
+++ ordersystem/products/api.py
@@ -4,16 +4,25 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+
 class ProductViewSet(viewsets.ModelViewSet):
 
     serializer_class = ProductSerializer
 
     def get_permissions(self):
+
         if self.request.method == 'GET':
             self.permission_classes = [permissions.IsAuthenticated]
         else:
-            self.permission_classes = [permissions.NOT]
-        return super(ProductViewSet, self).get_permissions()        
+            #Replace text surrounded with # with self.permission_classes = [permissions.IsAdminUser]
+            #This code is only to preserve the demo webside
+            #
+            if self.request.user.username == 'zampilled':
+                self.permission_classes = [permissions.IsAdminUser]
+            else:
+                self.permission_classes = [permissions.NOT]
+            #
+            return super(ProductViewSet, self).get_permissions()
 
     def get_queryset(self):
         return Product.objects.all()
